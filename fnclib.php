57ff6830af18f3f4bc8d69b6262063f0cc3ea069
danieljporter21 <danieljporter21@googlemail.com>
2022-04-04 11:57:29 +0100

    setup function lib

--- fnclib.php
+++ fnclib.php
@@ -1,1 +1,10 @@
 <?php
+
+foreach ($result as $film) {
+    $filmcomponent =
+        '<div>'
+        . '<h2>' . $film['title'] . '</h2>'
+        . '<img src="Images/' . $film['img_name'] . '">'
+        . '</div>';
+    echo $filmcomponent;
+}
