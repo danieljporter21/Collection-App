5f9b5515c3a07f84e0d14c59866e35a15d4fc422
danieljporter21 <danieljporter21@googlemail.com>
2022-04-04 16:33:36 +0100

    First view of collection

--- fnclib.php
+++ fnclib.php
@@ -4,9 +4,13 @@ function displayFilm($array)
 {
     foreach ($array as $film) {
         $filmcomponent =
-            '<div>'
+            '<div class="film">'
             . '<h2>' . $film['title'] . '</h2>'
             . '<img src="Images/' . $film['img_name'] . '">'
+            . '<p> Box Office: $' . $film['box_office'].'m</p>'
+            . '<p> Director: ' . $film['director'].'</p>'
+            . '<p> Phase: ' . $film['phase'].'</p>'
+            . '<p> Release Date: ' . $film['release_date'].'</p>'
             . '</div>';
         echo $filmcomponent;
     }
--- index.php
+++ index.php
@@ -4,7 +4,29 @@ require_once 'fnclib.php';
 
 $films = fetchAllFilms(connectToDB('CollectionApp'));
 
-echo displayFilm($films);
+?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>HTML with PHP</title>
+    <link rel="stylesheet" href="CSS/style.css">
+    <link rel="stylesheet" href="CSS/normalize.css">
+</head>
+<body>
+<header>
+    <div class="header">
+        <p>Marvel Film Collection</p>
+    </div>
+</header>
 
+<div class="box">
+    <?php
+    '<div class="box">' . displayFilm($films) . '</div>';
+    ?>
+</div>
+
+</body>
+</html>
 
