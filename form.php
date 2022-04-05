aad32b79e7e675a8d3a38b9d70f8ff3d72fce0dc
danieljporter21 <danieljporter21@googlemail.com>
2022-04-05 17:00:11 +0100

    Added Input Form

--- form.php
+++ form.php
@@ -1,1 +1,33 @@
 <?php
+
+require_once 'db.php';
+
+$db = 'CollectionApp';
+$connection = (connectToDB($db));
+
+function addToDB($pdo) {
+
+    $query = $pdo->prepare(
+        'INSERT INTO `films` (`title`,`box_office`, `director`, `phase`, `release_date`)'
+        . 'VALUES (:newTitle, :newBoxOffice, :newDirector, :newPhase, :newReleaseDate)'
+    );
+
+
+    $title = $_POST['title'];
+    $boxOffice = $_POST['box_office'];
+    $director = $_POST['director'];
+    $phase = $_POST['phase'];
+    $releaseDate= $_POST['release_date'];
+
+    $query->bindParam(':newTitle', $title);
+    $query->bindParam(':newBoxOffice', $boxOffice);
+    $query->bindParam(':newDirector', $director);
+    $query->bindParam(':newPhase', $phase);
+    $query->bindParam(':newReleaseDate', $releaseDate);
+
+    $query->execute();
+}
+
+addToDB($connection);
+
+header("Location: index.php");
