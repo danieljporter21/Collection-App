038e7ca5f627f819a1190c2415466ce37a590801
danieljporter21 <danieljporter21@googlemail.com>
2022-04-06 14:41:05 +0100

    Updated with required form entry

---
+++ tests/form.php
@@ -0,0 +1,53 @@
+<?php
+
+require_once '../fnclib.php';
+
+use PHPUnit\Framework\TestCase;
+
+class fnclib extends TestCase
+{
+    public function testGivenEmptyArray()
+    {
+        $array = [];
+
+        $result = displayFilms($array);
+
+        $this->assertEquals('', $result);
+    }
+
+    public function testGivenValidFilm()
+    {
+        $array[] = array(
+            'id' => 1,
+            'title' => 'Iron-Man',
+            'box_office' => 585.2,
+            'director' => 'John Favreau',
+            'phase' => 'One',
+            'release_date' => '2008-05-02',
+            'img_name' => 'iron_man1.jpg'
+        );
+
+        $result = displayFilms($array);
+
+        $this->assertEquals('<div class="film"><h2>Iron-Man</h2><img src="Images/iron_man1.jpg"><p> Box Office: $585.2m</p><p> Director: John Favreau</p><p> Phase: One</p><p> Release Date: 02-05-08</p></div>', $result);
+    }
+
+    public function testGivenStringThrowError()
+    {
+        //Arrange - setting up the data
+        $array = 'string';
+
+        $this->expectException(TypeError::class);
+
+        //Act - calling the function
+        $result = displayFilms($array);
+    }
+
+    public function testGivenDateReturnDate(){
+        $date = '21/07/1994';
+
+        $result = returnDate($date);
+
+        $this->assertEquals('', $result);
+    }
+}
